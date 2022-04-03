7494842b67cca8ea8b10d3e2700c117694216049
mrigankdoshy <mrigankdoshy@gmail.com>
2022-04-03 17:49:27 -0400

    Shared bullet

--- lib/widgets/recent_tech.dart
+++ lib/widgets/recent_tech.dart
@@ -1,8 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+
 import 'package:personal_website/utils/theme.dart';
-import 'package:personal_website/widgets/triangular_bullet.dart';
+import 'package:personal_website/widgets/bullet.dart';
 
 class RecentTech extends StatelessWidget {
   final String title;
@@ -17,16 +17,7 @@ class RecentTech extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Row(
         children: [
-          Transform.rotate(
-            angle: 90 * math.pi / 180,
-            child: CustomPaint(
-              painter: TriangularBullet(),
-              child: const SizedBox(
-                height: 8,
-                width: 10,
-              ),
-            ),
-          ),
+          const Bullet(),
           const SizedBox(width: 15),
           AutoSizeText(
             title,
