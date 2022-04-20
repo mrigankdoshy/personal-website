d4c88e9c6f7d40cef57a027cd1cd022eb3b302a6
mrigankdoshy <mrigankdoshy@gmail.com>
2022-04-19 21:34:57 -0400

    Remove unused imports

--- lib/widgets/menu_button.dart
+++ lib/widgets/menu_button.dart
@@ -1,6 +1,4 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:personal_website/data/text.dart';
 import 'package:personal_website/utils/theme.dart';
 
 class MenuButtton extends StatelessWidget {
