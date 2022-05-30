4f11bcc7604902522d3165ee5e145c04d0a50f2b
mrigankdoshy <mrigankdoshy@gmail.com>
2022-05-30 16:38:42 -0400

    Revert fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_website/home.dart';
 import 'package:personal_website/utils/theme.dart';
@@ -16,15 +12,11 @@ class PersonalWebsite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mrigank Doshy | Software Engineer",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         canvasColor: AppColors.backgroundBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        // TODO: https://github.com/flutter/flutter/issues/93140
-        fontFamily: kIsWeb && window.navigator.userAgent.contains('OS 15_')
-            ? '-apple-system'
-            : 'Calibre',
+        fontFamily: 'Calibre',
       ),
       home: const Home(),
     );
