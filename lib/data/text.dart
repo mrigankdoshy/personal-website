48eebe4072af046b709eb030b40c885fbe8af921
mrigankdoshy <mrigankdoshy@gmail.com>
2022-10-29 19:47:51 -0400

    Update version

--- lib/data/text.dart
+++ lib/data/text.dart
@@ -44,7 +44,7 @@ class FooterData {
   static const contactPart5 = ".";
 
   static const copyright = "© 2022 by Mrigank Doshy";
-  static const version = "v0.3.0";
+  static const version = "v1.0.0";
 }
 
 class IntroData {
